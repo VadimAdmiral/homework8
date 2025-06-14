de5cb3e398c60c86c79746820de74dd4e59752b0
helloworld <shchur.vadim.90@list.ru>
2025-06-14 11:10:23 +0300

    проверьте дз

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,13 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        //Задача 1;
+        // Задача 1
         System.out.println("Задача 1");
         int[] first = new int[]{1, 2, 3};
         float[] two = {1.57f, 7.654f, 9.986f};
         int[] free = new int[10];
-        //Задача 2;
+
+        // Задача 2
         System.out.println("Задача 2");
         for (int i = 0; i < first.length; i++) {
             if (i > 0) {
@@ -16,56 +17,45 @@ public class Main {
             System.out.print(first[i]);
         }
         System.out.println();
-        //Задача 3;
+
+        // Задача 3
         System.out.println("Задача 3");
-        int[] box = new int[3];
-        box[0] = 1;
-        box[1] = 2;
-        box[2] = 3;
-        double[] klop = {1.57, 7.654, 9.986};
-        int[] boll = {3, 12, 20};
-        System.out.println("задание 2");
-        for (int i = box.length - 1; i >= 0; i--) {
-            if (i > 0) {
-                System.out.print(box[i] + ", ");
-            } else {
-                System.out.print(box[i]);
+        int[] box = {3, 2, 1};
+        double[] klop = {9.986, 7.654, 1.57};
+        int[] boll = {20, 12, 3};
+
+        for (int i = 0; i < box.length; i++) {
+            System.out.print(box[i]);
+            if (i < box.length - 1) {
+                System.out.print(", ");
             }
         }
         System.out.println();
-                for (int i1 = 0; i1 < klop.length; i1++) {
-                    if (i1 < klop.length - 1) {
-                        System.out.print(klop[i1] + ", ");
-                    } else {
-                        System.out.print(klop[i1]);
-                        System.out.println();
-                        ///
-                        for (int i2 = 0; i2 < boll.length; i2++) {
-                            if (i2 < boll.length - 1) {
-                                System.out.print(boll[i2] + ", ");
-                            } else {
-                                System.out.print(boll[i2]);
-                            }
-                            for (int r = 0; r < boll.length; r++) {
-                                System.out.print(boll[r]);
-                                if (r < boll.length - 1) {
-                                    System.out.print(", ");
-                                    //Задача 4;
-                                    System.out.println("Задача 4");
-                                    int[] boxe = {1, 2, 3};
 
-                                    for (int iq = 0; iq < boxe.length; iq++) {
-                                        if (boxe[iq] % 2 != 0) {
-                                            boxe[iq] += 1;
-                                        }
-                                    }
-
-                                    System.out.println(Arrays.toString(box));
-                                }
-                            }
-                        }
-                    }
-                }
+        for (int i = 0; i < klop.length; i++) {
+            System.out.print(klop[i]);
+            if (i < klop.length - 1) {
+                System.out.print(", ");
             }
         }
+        System.out.println();
 
+        for (int i = 0; i < boll.length; i++) {
+            System.out.print(boll[i]);
+            if (i < boll.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        // Задача 4
+        System.out.println("Задача 4");
+        int[] boxe = {1, 2, 3};
+        for (int iq = 0; iq < boxe.length; iq++) {
+            if (boxe[iq] % 2 != 0) {
+                boxe[iq] += 1;
+            }
+        }
+        System.out.println(Arrays.toString(boxe));
+    }
+}
